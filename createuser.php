fcb368b5d1c517ffa29ae451dfaa29cf3d233d93
Sally Lee <sallylee@wustl.edu>
2022-02-18 11:11:00 -0600

    new users insert into database

--- createuser.php
+++ createuser.php
@@ -22,10 +22,80 @@ else {
     echo "valid username";
 }
 
-$sql = "select * from 'users' where name = '$user'";
-echo($sql); // this prints!
-$query = mysql_query($sql); // this does not :(
-echo(mysql_num_rows($query)); // this also does not :(
+//$sql = "select * from 'users' where name = '$user'";
+//echo($sql); // this prints!
+/*$stmt = $mysqli->prepare("select * from users where name = ?");
+if(!$stmt){
+	printf("Query Prep Failed: %s\n", $mysqli->error);
+	exit;
+}
+$stmt->bind_param('s', $user);
+
+$stmt->execute();
+
+$stmt->bind_result($res);
+
+$stmt->fetch();
+
+
+
+$stmt->close();
+*/
+
+//https://blog.arvixe.com/how-to-check-if-record-exists-in-sql-database-table-with-php/
+$q="SELECT COUNT(1) FROM users WHERE username=$user";
+$r=mysqli_query($mysqli, $q);
+$row=mysqli_fetch_row($r);
+//Now to check, we use an if() statement
+if($row[0] >= 1) 
+{
+    //print "Record exists";
+    echo "This username is already taken."; // prompt for password to just login!
+    ?>
+    
+    <form name ="input" action='login.php'>
+        <input type="submit" value="back to sign up" />
+    </form>
+    <?php
+    exit;
+} 
+else 
+{
+    //print "Record doesn't exist";
+    //echo "username not taken";
+
+    // hash password
+    $p_hash = password_hash($pass, PASSWORD_BCRYPT);
+
+    //add user to users table
+    //$stmt = $mysqli->prepare("insert into users (username, password) values ('sally', 'sally')"); WORKS
+    $stmt = $mysqli->prepare("insert into users (username, password) values (?, ?)");
+
+    if(!$stmt){
+        printf("Query Prep Failed: %s\n", $mysqli->error);
+        exit;
+    }
+    else {
+        echo "user added! " . $user; //this prints!
+    }
+
+    if(!$stmt->bind_param('ss', $user, $p_hash))
+    {
+        printf("Query bind Failed: %s\n", $mysqli->error);
+        exit;
+    }
+
+    if(!$stmt->execute())
+    {
+        printf("Query execute Failed: %s\n", $mysqli->error);
+        exit;
+    }
+
+    $stmt->close();
+
+}
+
+//echo(mysqli_num_rows($query)); // this also does not :(
 
 /*if(mysql_num_rows($query)){
         echo "This username is already taken."; // prompt for password to just login!
